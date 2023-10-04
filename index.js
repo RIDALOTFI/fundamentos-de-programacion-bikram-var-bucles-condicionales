b8b6f03b4c94f4c9cd3eb12689acc681893e6a15
RIDALOTFI <lotfirida1996@gmail.com>
2023-10-04 15:08:30 +0200

    versiónfinal test

---
+++ index.js
@@ -0,0 +1,56 @@
+
+let variableSinValor ;// variables
+let booleano1= true;
+let booleano2= false;
+const PI = 3.14;
+const TAU = 2*PI;
+
+let booleanoAnd= (booleano1==booleano2); // Booleanos
+let booleanoNot = (!booleano1);
+let booleanoMix0 = ((booleano1||booleano2)&&(booleano1||(!(booleano1&&(!booleano2)))))
+
+// Operadores
+
+
+let incrementarDesp = 2;
+let resultadoDesp=incrementarDesp++;
+let incrementarAntes=2;
+let resultadoAntes=++incrementarAntes;
+
+// Bucles 
+
+for (let contarHasta10_2= 0; contarHasta10_2 ===10; contarHasta10_2++){console.log(contarHasta10_2)}
+
+
+for(let postI=0,postJ=0;postI<=11;postI = postI + postJ++){console.log(postI)}
+
+//Iteracionea proyecto individual
+
+//Variables
+let variableValorNumerico = 100;
+const MiNombre= "moha";
+const MiNumeroFav = 12;
+
+//Booleanos
+let booleanoOr = (booleano1||booleano2);
+let booleanoMix1 = (((booleano1&&(TAU/2))==PI)||(variableValorNumerico>=MiNumeroFav))
+let seisNoEsNueve = (6!==9)
+let booleanoMix2=((variableValorNumerico>0) || (variableValorNumerico<-(MiNumeroFav*TAU)))
+
+//Operadores 
+
+let valorSuma = MiNumeroFav + variableValorNumerico
+let valorResta = MiNumeroFav-variableValorNumerico;
+let valorMultiplicacion = MiNumeroFav*variableValorNumerico;
+let valorDivision = MiNumeroFav/3;
+
+//Bucles
+
+let contarHasta10 = 0;
+
+while (contarHasta10!==10){contarHasta10++}
+
+
+
+
+
